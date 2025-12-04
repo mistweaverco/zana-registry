7488d8e7d7361940866e14f49cfcab2dc0a202d5
Marco Kellershoff <marco@mistweaverco.com>
2025-12-04 09:37:09 +0100

    feat(scripts): refactor and make stuff re-usable

--- scripts/convert-from-mason-package.ts
+++ scripts/convert-from-mason-package.ts
@@ -1,33 +1,25 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as yaml from 'js-yaml';
+import * as fs from "fs";
+import * as path from "path";
+import * as yaml from "js-yaml";
 
-interface PackageInfo {
-  name: string;
-  description: string;
-  homepage: string;
-  licenses: string[];
-  languages: string[];
-  categories: string[];
-  source: {
-    id: string;
-  };
-  bin: Record<string, string>;
-}
+import { getZanaYAMLHeader } from "./utils";
+import type { PackageInfo } from "./types";
+const packagesDir = path.join(__dirname, "..", "packages");
 
-const packagesDir = path.join(__dirname, '..', 'packages');
-
-fs.readdirSync(packagesDir, { withFileTypes: true }).forEach(dirent => {
+fs.readdirSync(packagesDir, { withFileTypes: true }).forEach((dirent) => {
   if (dirent.isDirectory()) {
-    const packageYamlPath = path.join(packagesDir, dirent.name, 'package.yaml');
+    const packageYamlPath = path.join(packagesDir, dirent.name, "package.yaml");
 
     if (fs.existsSync(packageYamlPath)) {
-      const fileContents = fs.readFileSync(packageYamlPath, 'utf8');
+      const fileContents = fs.readFileSync(packageYamlPath, "utf8");
       const packageData = yaml.load(fileContents) as PackageInfo;
-      packageData.source.id = packageData.source.id.replace(/@.*$/, '');
+      packageData.source.id = packageData.source.id.replace(/@.*$/, "");
       packageData.source.id = decodeURIComponent(packageData.source.id);
-      const zanaYamlPath = path.join(packagesDir, dirent.name, 'zana.yaml');
-      fs.writeFileSync(zanaYamlPath, yaml.dump(packageData));
+      const zanaYamlPath = path.join(packagesDir, dirent.name, "zana.yaml");
+      fs.writeFileSync(
+        zanaYamlPath,
+        getZanaYAMLHeader() + "\n" + yaml.dump(packageData),
+      );
       fs.unlinkSync(packageYamlPath);
       console.log(`Converted ${packageYamlPath} to ${zanaYamlPath}`);
     }
---
+++ scripts/types.ts
@@ -0,0 +1,79 @@
+import { type ErrorObject } from "ajv";
+
+export type GithubDataResponse = {
+  tag_name: string;
+};
+
+export type NpmDataResponse = {
+  version: string;
+};
+
+export type PyPiResponse = {
+  info: {
+    version: string;
+  };
+};
+
+export type GolangResponse = {
+  Version: string;
+};
+
+export type CrateResponse = {
+  crate: {
+    max_stable_version: string;
+  };
+};
+
+export interface MasonPackageInfo {
+  name: string;
+  description: string;
+  homepage: string;
+  licenses: string[];
+  languages: string[];
+  categories: string[];
+  source: {
+    id: string;
+  };
+  bin: Record<string, string>;
+}
+
+export type APIResponse = Promise<
+  | GithubDataResponse
+  | NpmDataResponse
+  | PyPiResponse
+  | GolangResponse
+  | CrateResponse
+  | null
+>;
+
+export interface PackageInfo {
+  name: string;
+  version: string;
+  description: string;
+  homepage: string;
+  licenses: string[];
+  languages: string[];
+  categories: string[];
+  experimental?: boolean;
+  source: {
+    id: string;
+  };
+  bin: Record<string, string>;
+}
+
+export interface PackageInfoWithValidationErrorsSupport extends PackageInfo {
+  validationErrors?: ErrorObject[];
+}
+
+export type SchemaErrors = {
+  name: string;
+  errors: ErrorObject[];
+};
+
+export enum SourceType {
+  GITHUB = "pkg:github",
+  NPM = "pkg:npm",
+  PYPI = "pkg:pypi",
+  GOLANG = "pkg:golang",
+  CARGO = "pkg:cargo",
+}
