2fb30acc1dd37a3078c77029b1f9825ee9fa5d78
Marco Kellershoff <marco@mistweaverco.com>
2026-05-08 11:18:53 +0200

    feat(web): enable filtering and add persisting links

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -1,15 +1,26 @@
 export interface Package {
-  name: string;
-  source: {
-    id: string;
-  };
-  description: string;
-  version: string;
-  homepage: string;
-  licenses: string[];
-  languages?: string[];
-  tags?: string[];
-  categories: string[];
-  aliases?: string[];
-  searchMatchInfo?: string;
+	name: string;
+	source: {
+		id: string;
+	};
+	description: string;
+	version: string;
+	homepage: string;
+	licenses: string[];
+	languages?: string[];
+	tags?: string[];
+	categories: string[];
+	aliases?: string[];
+	searchMatchInfo?: string;
+	treesitter?: {
+		build: {
+			language: string;
+			grammar_dir: string;
+		}[];
+	};
+}
+
+export enum PackageTreesitterIntegration {
+	None = 'none',
+	Neovim = 'neovim'
 }
